b802b5043894a9863c030b36e42ff2a5d295214b
Veronika Bosáková <veronikabosakova@gmail.com>
2022-04-12 19:02:41 +0200

    [ENHANCEMENT] propagate BMI result to UI

--- bmi-input.js
+++ bmi-input.js
@@ -7,6 +7,13 @@ function vypocitejBMI() {
   let BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
 
   console.log(BMI);
+
+  vypisBMI(BMI);
+}
+
+// funkce pro zapsání BMI do UI
+function vypisBMI(BMI) {
+  document.querySelector("#vysledek > span").innerHTML = BMI;
 }
 
 document.querySelectorAll("input").forEach((element) => {
